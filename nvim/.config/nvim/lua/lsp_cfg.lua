6ef03dfdbd288ccff455937b62233a438b14dadb
sl8vz <37440531+sl8vz@users.noreply.github.com>
2020-12-20 14:17:27 +0100

    nvim: lsp: config update
    Align with nvim changes.
    Better mapping
    
    Signed-off-by: sl8vz <37440531+sl8vz@users.noreply.github.com>

--- nvim/.config/nvim/lua/lsp_cfg.lua
+++ nvim/.config/nvim/lua/lsp_cfg.lua
@@ -1,34 +1,40 @@
 local map = function(type, key, value)
-	vim.fn.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
+  vim.fn.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
 end
 
 local custom_attach = function(client)
-	print("LSP started.");
-	require'completion'.on_attach(client)
-	require'diagnostic'.on_attach(client)
+  print("LSP started.");
+  require'completion'.on_attach(client)
 
-	map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
-	map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
---	map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
-	map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
-	map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
-	map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
-	map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
-	map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-	map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
-	map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
-	map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
-	map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
-	map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
-	map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-	map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
-	map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+  map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+  map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+  map('n','<space>','<cmd>lua vim.lsp.buf.hover()<CR>')
+  map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+  map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+  map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+  map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+  map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+  map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+  map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+  map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+  map('n','<leader>ee','<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+  map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+  map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+  map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+  map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
 end
 
-local lsp = require'nvim_lsp'
+local lsp = require'lspconfig'
 
-lsp.tsserver.setup{on_attach=custom_attach}
-lsp.clangd.setup{
-	cmd = {"clangd", "--log=verbose", "--background-index", "--query-driver=/home/seb/build/toolchains/**/bin/arm-none-eabi-*, /home/seb/build/toolchains/*_aarch64/sysroots/x86_64-pokysdk-linux/usr/bin/aarch64-poky-linux/aarch64-poky-linux-*, /home/seb/git/openil/output/host/bin/aarch64-none-linux-gnu-*"};
-	on_attach=custom_attach
-}
+lsp.bashls.setup{on_attach=custom_attach}
+lsp.ccls.setup{on_attach=custom_attach}
+lsp.sumneko_lua.setup{on_attach=custom_attach}
+lsp.cmake.setup{on_attach=custom_attach}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = false,
+    virtual_text = false,
+    update_in_insert = false,
+  }
+)
